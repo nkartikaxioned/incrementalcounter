cb00d647b764ba4f3d93d4ce492d84c1d6ac48de
nkartikaxioned <kartikn@axioned.com>
2023-10-02 15:20:32 +0530

    js add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,30 @@
 
 */
 
+const numCounter=document.querySelectorAll('.counter');
+
+numCounter.forEach((element) => {
+
+const numberIncrement=() =>{
+  const targetNum=parseInt(element.dataset.target)
+  let start=parseInt(element.innerHTML)
+
+  if(start<targetNum) {
+   let interval=setInterval(() => {
+   start+=10;
+   element.innerText=`${start}`;
+
+   if(start===targetNum) {
+    clearInterval(interval)
+
+   }
+   })
+  }
+}
+
+numberIncrement();
+})
+
 
 
 
